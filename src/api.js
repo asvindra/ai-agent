60a8765c89d31d92ec7172303569fe1b6f7e5eb5
Asvindra Rajpoot <arajpoot@juniper.net>
2025-02-21 20:26:42 +0530

    Added basic setup

---
+++ src/api.js
@@ -0,0 +1,30 @@
+const BASE_URL = "http://localhost:3001";
+
+async function createChat() {
+  const res = await fetch(BASE_URL + "/chats", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+  });
+  const data = await res.json();
+  if (!res.ok) {
+    return Promise.reject({ status: res.status, data });
+  }
+  return data;
+}
+
+async function sendChatMessage(chatId, message) {
+  const res = await fetch(BASE_URL + `/chats/${chatId}`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ message }),
+  });
+  if (!res.ok) {
+    return Promise.reject({ status: res.status, data: await res.json() });
+  }
+  return res.body;
+}
+
+export default {
+  createChat,
+  sendChatMessage,
+};
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import {
+  FaTachometerAlt,
+  FaComments,
+  FaCog,
+  FaQuestionCircle,
+  FaSignInAlt,
+} from "react-icons/fa";
+
+const Sidebar = () => {
+  return (
+    <div className="h-full w-20 fixed top-0 left-0 bg-gray-900 p-5">
+      <h2 className="text-white text-center text-2xl mb-5">AI</h2>
+      <ul className="list-none p-0">
+        <li className="p-2 text-center text-white hover:bg-gray-700 cursor-pointer">
+          <FaComments className="mx-auto mb-1" />
+        </li>
+        <li className="p-2 text-center text-white hover:bg-gray-700 cursor-pointer">
+          <FaCog className="mx-auto mb-1" />
+        </li>
+        <li className="p-2 text-center text-white hover:bg-gray-700 cursor-pointer">
+          <FaQuestionCircle className="mx-auto mb-1" />
+        </li>
+        <li className="p-2 text-center text-white hover:bg-gray-700 cursor-pointer">
+          <FaSignInAlt className="mx-auto mb-1" />
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Sidebar;
